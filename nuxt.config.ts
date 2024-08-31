e008eafba291ba89c305821748d1edb6c01639a5
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-08-31 19:32:54 +0700

    finish adding vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
+  app: {
+    head: {
+      // link: [
+      //   {
+      //     rel: 'stylesheet',
+      //     href: "https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+      //   }
+      // ]
+      // script: [
+      //   {
+      //     src: 'https://kit.fontawesome.com/ae224cd0da.js',
+      //     crossorigin: 'anonymous'
+      //   }
+      // ]
+    }
+  },
+
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: [(_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      // @ts-expect-error
+      config.plugins.push(vuetify({ autoImport: true }))
+    })
+  },'@nuxt/content'],
 
   routeRules: {
     '/': { prerender: true }
   },
 
-  compatibilityDate: '2024-08-31'
+  compatibilityDate: '2024-08-31',
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
