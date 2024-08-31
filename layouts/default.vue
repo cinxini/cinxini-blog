ef2e4a14ed7b8371975a953b37fc646fc248c1de
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-08-31 22:16:18 +0700

    styling nav-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar color="primary">
-      <v-app-bar-title>Cinxini's Dev Blog</v-app-bar-title>
+    <v-app-bar id="navbar" color="background" :elevation="0" height="50">
+      <!-- <v-app-bar-title>Cinxini's Dev Blog</v-app-bar-title> -->
+      <template v-slot:prepend>
+        <v-img :width="120" aspect-ratio="16/9" cover src="@/assets/images/logo.png"></v-img>
+      </template>
       <template v-slot:append>
-        <v-btn nuxt to="/">
-          Home
+        <v-btn v-for="item in navItems" :key="item.path" nuxt class="nav-btn mx-1" color="base" active-color="primary"
+          :to="item.path">
+          {{ item.title }}
         </v-btn>
-        <v-btn nuxt to="/about">
-          About
-        </v-btn>
-        <v-btn nuxt to="/posts">
-          Blog
-        </v-btn>
-        <v-btn icon="fa-solid fa-magnifying-glass"></v-btn>
+        <v-btn icon="fa-solid fa-magnifying-glass" size="small" color="base"></v-btn>
       </template>
     </v-app-bar>
-    <v-main>
+    <v-main style="padding-top: 50px;">
       <slot />
     </v-main>
 
@@ -23,6 +21,23 @@
 </template>
 
 <script setup>
+const navItems = [
+  { title: 'Home', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Blog', path: '/posts' },
+]
 </script>
 
-<style></style>
+<style>
+:root {
+  --heading-border-opacity: 0.3;
+}
+
+#navbar {
+  border-bottom: 1px solid rgba(var(--v-theme-base), var(--heading-border-opacity));
+}
+
+#navbar .nav-btn {
+  font-weight: bold;
+}
+</style>
