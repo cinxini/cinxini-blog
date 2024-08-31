0e9855c08b329c61ad12f33fd8879e7e7c828fd8
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-08-31 16:22:47 +0700

    enable nuxt layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <template>
+    <div>
+      <p>This is a default layout page.</p>
+      <slot />
+    </div>
+  </template>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
