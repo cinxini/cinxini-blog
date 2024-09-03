894c96d22d3731f19cb16a3b137276ad6fb4feaf
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-09-03 18:30:58 +0700

    update

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,10 @@
 <template>
   <v-container id="main-content" :width="containerWidth">
     <SectionTitle title="Recent Blog Posts" icon="fa-solid fa-quote-right" />
-    <ContentQuery path="/blog" v-slot="{ data }" :where="{ draft: false }" :sort="{ 'dates.published': -1 }" :limit="2"
-      :skip="skip">
-      <!-- <pre>{{ data }}</pre> -->
-      <!-- <ContentRenderer :value="data" /> -->
-      <ContentList :articles="data" class="mt-10" />
-    </ContentQuery>
-    <!-- <ContentList :articles="posts" class="mt-10" /> -->
-    <v-pagination :length="numPages" v-model="currPage" next-icon="fa-solid fa-caret-right"
-      prev-icon="fa-solid fa-caret-left" rounded="lg" color="base" active-color="primary"
-      "></v-pagination>
+    <ContentList :articles="posts" class="mt-10" />
+    <v-pagination nuxt :length="numPages" v-model="currPage" next-icon="fa-solid fa-caret-right"
+      prev-icon="fa-solid fa-caret-left" rounded="lg" color="base" active-color="primary" :previous="refresh()"
+      :next="refresh()"></v-pagination>
   </v-container>
 </template>
 
@@ -23,15 +17,15 @@ import { computed, ref } from 'vue';
 const viewport = useViewport()
 const appConfig = useAppConfig()
 const route = useRoute()
-console.log(route)
+
 const { width: containerWidth } = useNuxtDisplay(viewport.breakpoint);
-const currPage = ref(route.query.page? Number(route.query.page): 1);
+const currPage = ref(route.query.page ? Number(route.query.page) : 1);
 
 const fetchPosts = async (pageNo) => {
   return queryContent('/blog')
     .where({ draft: false })
     .sort({ 'dates.published': -1 })
-    .skip((pageNo - 1) * appConfig.maxPostPerPage)
+    .skip(skip.value)
     .limit(appConfig.maxPostPerPage)
     .find()
 }
@@ -39,7 +33,7 @@ const fetchPosts = async (pageNo) => {
 const skip = computed(() => {
   return (currPage.value - 1) * appConfig.maxPostPerPage
 })
-const { data: posts } = await useAsyncData('blogPostList', async () => {
+const { data: posts, refresh } = await useAsyncData('blogPostList', async () => {
   console.log('call')
   return fetchPosts(currPage.value);
 })
@@ -54,22 +48,6 @@ const numPages = computed(() => {
   if (numPosts.value === 0 || numPosts.value < appConfig.maxPostPerPag) return 1;
   return Math.ceil(numPosts.value / appConfig.maxPostPerPage);
 })
-
-watch(currPage, async (newPage) => {
-  console.log(newPage)
-  await navigateTo('cinxini.com/posts?page=' + newPage)
-})
-
-const gotoPage = async (pageNo) => {
-  // let link = document.createElement('a');
-
-  // link.href = `${route.path}?page=${pageNo}`;
-  // link.click()
-  console.log(pageNo)
-  const data = await fetchPosts(pageNo);
-  posts.value = data;
-
-}
 </script>
 
 <style></style>
