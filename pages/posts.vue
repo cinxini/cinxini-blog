dc427a53aee7fea7922cb4abf0ceee7acd1338bb
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-09-03 17:18:41 +0700

    Update posts.vue

--- pages/posts.vue
+++ pages/posts.vue
@@ -3,7 +3,8 @@
     <SectionTitle title="Recent Blog Posts" icon="fa-solid fa-quote-right" />
     <ContentList :articles="posts" class="mt-10" />
     <v-pagination :length="numPages" v-model="currPage" next-icon="fa-solid fa-caret-right"
-      prev-icon="fa-solid fa-caret-left" rounded="lg" color="base" active-color="primary"></v-pagination>
+      prev-icon="fa-solid fa-caret-left" rounded="lg" color="base" active-color="primary"
+      @input="gotoPage(currPage)"></v-pagination>
   </v-container>
 </template>
 
@@ -11,21 +12,25 @@
 import ContentList from '@/components/containers/ContentList.vue';
 import SectionTitle from '@/components/display/SectionTitle.vue';
 import { useNuxtDisplay } from '@/composables/nuxtDisplay';
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 const viewport = useViewport()
 const appConfig = useAppConfig()
 const route = useRoute()
-
+console.log(route)
 const { width: containerWidth } = useNuxtDisplay(viewport.breakpoint);
-const currPage = ref(route.query.page ? Number(route.query.page) : 1);
+const currPage = ref(1);
 
-const { data: posts } = await useAsyncData('blogPostList', async () => {
+const fetchPosts = async (pageNo) => {
   return queryContent('/blog')
     .where({ draft: false })
     .sort({ 'dates.published': -1 })
-    .skip((currPage.value - 1) * appConfig.maxPostPerPage)
+    .skip((pageNo - 1) * appConfig.maxPostPerPage)
     .limit(appConfig.maxPostPerPage)
     .find()
+}
+
+const { data: posts } = await useAsyncData('blogPostList', async () => {
+  return fetchPosts(currPage.value);
 })
 
 const { data: numPosts } = await useAsyncData('blogPostCount', () => {
@@ -39,14 +44,22 @@ const numPages = computed(() => {
   return Math.ceil(numPosts.value / appConfig.maxPostPerPage);
 })
 
-watch(currPage, (newPage) => {
-  gotoPage(newPage)
+watch(currPage, async (newPage) => {
+  console.log(newPage)
+  const data = await fetchPosts(newPage);
+  posts.value = data
+  console.log(data)
 })
 
-const gotoPage = (pageNo) => {
-  let link = document.createElement('a');
-  link.href = `${route.path}?page=${pageNo}`;
-  link.click()
+const gotoPage = async (pageNo) => {
+  // let link = document.createElement('a');
+
+  // link.href = `${route.path}?page=${pageNo}`;
+  // link.click()
+  console.log(pageNo)
+  const data = await fetchPosts(pageNo);
+  posts.value = data;
+
 }
 </script>
 
